beeeb49ab1047ba6e92fb0a6c1ffb636317229e1
Inder <85822513+inderrr@users.noreply.github.com>
2024-11-28 20:45:50 +0530

    feat(check_updates.py): generate diff

--- update/check_updates.py
+++ update/check_updates.py
@@ -1,40 +1,9 @@
 import requests
-import yaml
 import json
 import os
-
-
-def parse_yaml(file):
-    return yaml.safe_load(file)
-
-
-def extract_image_tags(data: dict):
-    extracted = {}
-    services = data["services"]
-    for key in services:
-        extracted[key] = services[key]["image"]
-
-    return extracted
-
-
-def send_webhook(url: str, description: str):
-    try:
-        res = requests.post(
-            url,
-            json={
-                "content": "",
-                "embeds": [
-                    {
-                        "title": "Report",
-                        "description": description,
-                    }
-                ],
-            },
-        )
-
-        res.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        raise SystemExit(err)
+import io
+import datetime
+from difflib import HtmlDiff
 
 
 if __name__ == "__main__":
@@ -45,42 +14,42 @@ if __name__ == "__main__":
             "Error: discord_webhook_url env var not present",
         )
 
-    with open("docker/docker-compose.yml") as stream:
-        try:
-            local_img_tags = extract_image_tags(parse_yaml(stream))
+    current_dir = os.path.dirname(os.path.abspath(__file__))
 
+    with open(os.path.join(current_dir, "../docker/docker-compose.yml")) as local_file:
+        try:
             response = requests.get(
                 "https://raw.githubusercontent.com/supabase/supabase/refs/heads/master/docker/docker-compose.yml"
             )
 
-            remote_img_tags = extract_image_tags(parse_yaml(response.text))
+            d = HtmlDiff()
 
-            extra_services, upgradable = [], []
-
-            # one extra in local is caddy
-            if abs(len(remote_img_tags) - len(local_img_tags)) > 1:
-                for key in remote_img_tags:
-                    if key not in local_img_tags:
-                        extra_services.append(key)
-
-            for key in local_img_tags:
-                if key == "caddy":
-                    continue
-
-                if local_img_tags[key] != remote_img_tags[key]:
-                    upgradable.append(key)
-
-            send_webhook(
-                discord_webhook_url,
-                json.dumps(
-                    {
-                        "extra_services": extra_services,
-                        "upgradable": upgradable,
-                    },
-                    indent=0,
-                ),
+            html_diff = d.make_file(
+                local_file.read().splitlines(),
+                response.text.splitlines(),
+                fromdesc="Local File",
+                todesc="Remote File",
             )
-        except yaml.YAMLError as err:
-            raise SystemExit(f"Error parsing yaml: {err}")
+
+            report_date = datetime.datetime.now().strftime("%d-%m-%Y")
+            file = io.StringIO(html_diff)
+            file.name = f"diff-{report_date}.html"
+
+            try:
+                res = requests.post(
+                    discord_webhook_url,
+                    data={
+                        "payload_json": json.dumps(
+                            {"embeds": [{"title": f"Report {report_date}"}]}
+                        )
+                    },
+                    files={"file": file},
+                )
+                res.raise_for_status()
+            except requests.exceptions.HTTPError as err:
+                raise SystemExit(err)
+            finally:
+                file.close()
+
         except Exception as err:
             raise SystemExit(f"Error: {err}")

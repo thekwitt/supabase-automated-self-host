3f9dd30ed5dbea8e4c0d7964a28e41b9728a2b39
Inder <85822513+inderrr@users.noreply.github.com>
2024-12-01 03:27:11 +0530

    fix(setup.sh): add check for "__" in tokens
    feat(docker-compose.yml):instead of adding an extra env var in .env file, pass SUPABASE_PUBLIC_URL to caddy

--- setup.sh
+++ setup.sh
@@ -6,35 +6,27 @@ if [ "$EUID" -ne 0 ]; then
     exit 1
 fi
 
-# UPGRADE PACKAGES
-apt update && apt upgrade -y && apt install -y apache2-utils jq openssl
+apt update && apt upgrade -y && apt install -y apache2-utils jq openssl git
 
-# SET UP FIREWALL
-# echo "Setting up ufw"
-# ufw --force enable
-# ports=("ssh" "http" "https")
-# for port in "${ports[@]}"; do ufw allow "$port"; done
-
-echo "Setting up Docker"
+echo "Setting up docker"
 
 # https://stackoverflow.com/a/677212
-if ! command -v /usr/bin/docker >/dev/null 2>&1; then
-    curl -fsSL https://get.docker.com | sh >/dev/null
-
-    # https://stackoverflow.com/a/36131231
-    if [ -z "$(getent group docker)" ]; then
-        groupadd docker
-    fi
-
-    usermod -aG docker "$USER"
+if ! command -v /usr/bin/docker >/dev/null; then
+    curl -fsSL https://get.docker.com | sh
 else
     echo "docker already installed. skipping installation"
 fi
 
-git clone https://github.com/singh-inder/supabase-self-host
+directory="supabase"
 
-if ! cd supabase/docker; then
-    echo "Unable to access supabase/docker directory"
+if [[ -d "$directory" ]]; then
+    echo "$directory directory present, skipping git clone"
+else
+    git clone https://github.com/singh-inder/supabase-self-host "$directory"
+fi
+
+if ! cd "$directory"/docker; then
+    echo "Unable to access $directory/docker directory"
     exit 1
 fi
 
@@ -57,7 +49,7 @@ dashboardPassword=$(htpasswd -bnBC 12 "" "$dashboardPassword" | cut -d : -f 2)
 
 # https://www.willhaley.com/blog/generate-jwt-with-bash/
 
-jwt_secret=$(openssl rand -hex 40)
+jwt_secret=$(openssl rand -hex 20)
 
 base64_encode() {
     declare input=${1:-$(</dev/stdin)}
@@ -77,21 +69,11 @@ hmacsha256_sign() {
 
 header='{"typ": "JWT","alg": "HS256"}'
 header_base64=$(echo "${header}" | json | base64_encode)
+# iat and exp for both tokens has to be same thats why initializing here
 iat=$(date +%s)
-exp=$(("$iat" + 4 * 3600 * 24 * 365)) # 4 years expiry
+exp=$(("$iat" + 5 * 3600 * 24 * 365)) # 5 years expiry
 
 gen_token() {
-    # local payload=$(
-    #     # 4 years expiry
-    #     echo "$1" | jq --arg time_str "$(date +%s)" \
-    #         '
-    # ($time_str | tonumber) as $time_num
-    # | .iat=$time_num
-    # | .exp=($time_num + ((4*3600*24*365)))
-    # '
-    # )
-
-    # use the same iat and exp for anon_token and service_role_token
     local payload=$(
         echo "$1" | jq --arg jq_iat "$iat" --arg jq_exp "$exp" '.iat=($jq_iat | tonumber) | .exp=($jq_exp | tonumber)'
     )
@@ -105,8 +87,21 @@ gen_token() {
     echo "${header_payload}.${signature}"
 }
 
-anon_token=$(gen_token '{"role": "anon", "iss": "supabase"}')
-service_role_token=$(gen_token '{"role": "service_role", "iss": "supabase"}')
+anon_payload='{"role": "anon", "iss": "supabase"}'
+anon_token=$(gen_token "$anon_payload")
+
+service_role_payload='{"role": "service_role", "iss": "supabase"}'
+service_role_token=$(gen_token "$service_role_payload")
+
+# When double underscores ("__") are present in token, realtime analytics container remains unhealthy. Could be wrong about this behavior
+# my guess it maybe something to do with how env's are passed by docker compose
+# would say about 5/100 odds to have __ in token. for those 5 instances this loop:
+while [[ "${anon_token}" == *"__"* || "${service_role_token}" == *"__"* ]]; do
+    iat=$(date +%s)
+    exp=$(("$iat" + 5 * 3600 * 24 * 365)) # 5 years expiry
+    anon_token=$(gen_token "$anon_payload")
+    service_role_token=$(gen_token "$service_role_payload")
+done
 
 sed -e "s|POSTGRES_PASSWORD.*|POSTGRES_PASSWORD=$(openssl rand -hex 16)|" \
     -e "s|JWT_SECRET.*|JWT_SECRET=$jwt_secret|" \
@@ -114,8 +109,6 @@ sed -e "s|POSTGRES_PASSWORD.*|POSTGRES_PASSWORD=$(openssl rand -hex 16)|" \
     -e "s|SERVICE_ROLE_KEY.*|SERVICE_ROLE_KEY=$service_role_token|" \
     -e "s|API_EXTERNAL_URL.*|API_EXTERNAL_URL=$domain/api|" \
     -e "s|SUPABASE_PUBLIC_URL.*|SUPABASE_PUBLIC_URL=$domain|" ".env.example" >.env
-
-echo -e "\nDOMAIN=$domain" >>.env
 
 echo -e "{\$DOMAIN} {
         @api path /rest/v1/* /auth/v1/* /realtime/v1/* /storage/v1/* /api*
